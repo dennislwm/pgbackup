5aa057be70b76650ff13199474ca6c896d6b1e49
Dennis <Lee>
2021-04-15 14:32:22 +0800

    TDD for AWS S3 storage.py
    Additions:
    - boto3 package
    
    Updates:
    - src/pgbackup/storage.py
    - tests/test_storage.py

--- tests/test_storage.py
+++ tests/test_storage.py
@@ -1,17 +1,31 @@
 import tempfile
+import pytest
 
 from pgbackup import storage
 
-def test_storing_file_locally():
+@pytest.fixture
+def infile():
+    file = tempfile.TemporaryFile('r+b')
+    file.write(b"Testing")
+    file.seek(0)
+    return file
+
+def test_storing_file_locally(infile):
     """
     Writes content from one file-like to another
     """
-    infile = tempfile.TemporaryFile('r+b')
-    infile.write(b"Testing")
-    infile.seek(0)
-
     outfile = tempfile.NamedTemporaryFile(delete=False)
     storage.local(infile, outfile)
     with open(outfile.name, 'rb') as file:
         assert file.read() == b"Testing"
+
+def test_storing_file_on_s3(mocker, infile):
+    """
+    Writes content from one readable to S3
+    """
+    client = mocker.Mock()
+
+    storage.s3(client, infile, "bucket", "file-name")
+
+    client.upload_fileobj.assert_called_with(infile, "bucket", "file-name")
 
